215954ab53365fefc0d5807788c3b8d76662baf2
Felipe <felipe.s.carvalho_qui@hotmail.com>
2023-03-20 08:28:55 -0700

    call both C and C++ from .f90

--- test.f90
+++ test.f90
@@ -1,33 +1,24 @@
 program main
 
 ! Calling from C
-!    use iso_c_binding, only : c_int
-!
-!    interface
-!        subroutine say_hello(the_answer) bind( C, name = "say_hello_c")
-!            use iso_c_binding, only : c_int
-!            integer(c_int), value :: the_answer
-!            end subroutine
-!    end interface
-!
-!    integer ( c_int ), parameter :: the_answer = 3
-!
-!    call say_hello(the_answer)
-       
-! Calling from C++
     use iso_c_binding, only : c_int
 
     interface
-        subroutine say_hello(the_answer) bind( C, name = "say_hello_cpp")
-            use iso_c_binding, only : c_int
+        subroutine say_hello_c(the_answer) bind( C, name = "say_hello_c")
+                use iso_c_binding, only : c_int
+                integer(c_int), value :: the_answer
+        end subroutine say_hello_c
+
+        subroutine say_hello_cpp(the_answer) bind( C, name = "say_hello_cpp")
+             use iso_c_binding, only : c_int
             integer(c_int), value :: the_answer
-            end subroutine say_hello
+        end subroutine say_hello_cpp
     end interface
 
-    integer ( c_int ), parameter :: the_answer = 5
+    integer ( c_int ), parameter :: the_answer = 3
 
-    call say_hello(the_answer)
+    call say_hello_c(the_answer)
 
-! (to do) Calling from c++ which calls CUDA kernel
+    call say_hello_cpp(the_answer)
 
 end program
