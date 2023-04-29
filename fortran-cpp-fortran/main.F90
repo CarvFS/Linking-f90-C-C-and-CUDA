1c6c729d04385039fa3b739fced011b5d3d266a5
Felipe_SC <100419439+CarvFS@users.noreply.github.com>
2023-04-28 17:29:11 -0700

    Update main.F90

--- fortran-cpp-fortran/main.F90
+++ fortran-cpp-fortran/main.F90
@@ -1,10 +1,12 @@
 program main_program
     use iso_c_binding
-    use interface_mod
+    use SimpleF_mod
     implicit none
     integer :: a
     type(simp) :: s
     a = 123    
-    call Set_A(s,a)
-    !write(*,*) get_a(s)
+    call SimpleF__new(s)
+    write(*,*) "a = ", SimpleF__getA(s)
+    ! call C_SimpleF__setA(s, a)
+    ! write(*,*) SimpleF__getA(s)
 end program main_program
