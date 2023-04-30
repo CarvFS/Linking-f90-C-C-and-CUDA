bde41f2a3e6701cbcd1df8cecbce953a19c41ad8
Felipe_SC <100419439+CarvFS@users.noreply.github.com>
2023-04-30 11:53:20 -0700

    Update main.90 and write messages to track progress

--- fortran-cpp-fortran/main.F90
+++ fortran-cpp-fortran/main.F90
@@ -4,9 +4,13 @@ program main_program
     implicit none
     integer :: a
     type(simp) :: s
-    a = 123    
+    a = 4321 
+    write(*,*) "In main.90: creating object"
     call SimpleF__new(s)
+    write(*,*) "In main.90: print value of a defined at Simple_mod.F90..."
     write(*,*) "a = ", SimpleF__getA(s)
-    ! call C_SimpleF__setA(s, a)
-    ! write(*,*) SimpleF__getA(s)
+    write(*,*) "In main.90: defining new value for a..."
+    call SimpleF__setA(s, a)
+    write(*,*) "In main.90: print new value for a..."
+    write(*,*) "new a is... ", SimpleF__getA(s)
 end program main_program
