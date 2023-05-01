bc7cd7bd3ef5166c0a62f1306f34afbd159ba241
Felipe_SC <100419439+CarvFS@users.noreply.github.com>
2023-05-01 14:15:35 -0700

    Update main.cpp

--- fortran-cpp-fortran/main.cpp
+++ fortran-cpp-fortran/main.cpp
@@ -12,7 +12,8 @@ extern "C" void Setdk(void* handle, double dk);
 extern "C" double Getdk(void* handle);
 extern "C" void SetB(void* handle, const int* data, int data_size);
 extern "C" void QueryBSize(void* handle, int* data_size);
-extern "C" void QueryBData(void *handle, int *data);
+// extern "C" void QueryBData(void *handle, int *data);
+extern "C" void QueryBData(void *handle, int data[2]);
 extern "C" double add_a_dk(void* handle);
 
 class SimpleF
@@ -20,10 +21,12 @@ class SimpleF
 private:
   void *handle;
 public:
-  SimpleF() 
+  SimpleF(int a) 
   { 
     cout << "In main.cpp: Hello from SimpleF class constructor" << endl;
+    cout << "receiving a = " << a << endl;
     handle = GetHandle(); 
+    ::SetA(handle, a);
   }
 
   ~SimpleF() 
@@ -51,21 +54,30 @@ public:
     return ::Getdk(handle);
   }
 
-  void SetB(const vector<int>& b)
+  // void SetB(const vector<int>& b)
+  // {
+  //    ::SetB(handle, &b[0], b.size());
+  // }
+  void SetB(int n, int b[])
   {
-     ::SetB(handle, &b[0], b.size());
+     ::SetB(handle, b, n);
   }
 
-  vector<int> QueryB()
-  {
-    // Get the data size, construct a suitable buffer, populate the buffer.
-    int data_size;
-    ::QueryBSize(handle, &data_size);
-    if (data_size == 0) return vector<int>();
+  // vector<int> QueryB()
+  // {
+  //   // Get the data size, construct a suitable buffer, populate the buffer.
+  //   int data_size;
+  //   ::QueryBSize(handle, &data_size);
+  //   if (data_size == 0) return vector<int>();
 
-    vector<int> data(data_size);
-    ::QueryBData(handle, &data[0]);
-    return data;
+  //   vector<int> data(data_size);
+  //   ::QueryBData(handle, &data[0]);
+  //   return data;
+  // }
+
+  void QueryB(int n, int b[]){
+    cout << "In main.cpp: Hello from get B" << endl;
+    ::QueryBData(handle,b);
   }
 
   double add_a_dk(){
@@ -76,9 +88,9 @@ public:
 
 // F -> C++
 extern "C" {
-  SimpleF* SimpleF__new(){
+  SimpleF* SimpleF__new(int a){
     cout << "In main.cpp: Hello from Simple__new in cpp" << endl;
-    return new SimpleF();
+    return new SimpleF(a);
   }
   
   void SimpleF__setA(SimpleF* obj, int a){
@@ -88,6 +100,23 @@ extern "C" {
 
   int SimpleF__getA(SimpleF* obj){
     return obj->QueryA();
+  }
+
+  void SimpleF__setB(SimpleF* obj, int n, int b[]){
+    cout << "In main.cpp's SimpleF_setB: receiving b[0] = " << b[0] << "; b[1] = " << b[1] << endl;
+    obj -> SetB(n,b);
+  }
+
+  // vector<int> SimpleF__getB(SimpleF* obj, int n){
+  //   cout << "receiving n = " << n << endl;
+  //   return obj->QueryB();
+  // }
+  void SimpleF__getB(SimpleF* obj, int n, int b[2]){
+    cout << "In main.cpp: receiving n = " << n << endl;
+    cout << "In main.cpp: receiving b junk values as " << b[0] << " and " << b[1] << endl;
+    // return obj->QueryB();
+    obj -> QueryB(n,b);
+    cout << "In main.cpp: Returning previously defined values of b as " << b[0] << " and " << b[1] << endl;
   }
   
 }
