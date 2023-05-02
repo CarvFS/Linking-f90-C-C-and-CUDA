eae0b04b1000f7df0ab85f92c4eb5472ab0e48c6
Felipe_SC <100419439+CarvFS@users.noreply.github.com>
2023-05-02 11:41:16 -0700

    Set string in SimpleF__new

--- fortran-cpp-fortran/main.F90
+++ fortran-cpp-fortran/main.F90
@@ -4,19 +4,26 @@ program main_program
     implicit none
     integer :: a
     integer :: new_a
+    CHARACTER(256) :: Fstr = "Test for passing string F-Cpp-F"
+    CHARACTER(256) :: Cstr
     integer :: b(2), b2(2)
     type(simp) :: s
     b(1) = 12345
     b(2) = 54321
-    write(*,*) "b = ", b
+    ! write(*,*) "b = ", b
     
+    call F2Cstring(Fstr,Cstr)
+
     write(*,*) "In main.90: !!!!! CREATING OBJECT !!!!!"
-    call SimpleF__new(s,765)
+    write(*,*) "defining Fstr as: ", Cstr
+    call SimpleF__new(s,7654,Cstr)
 
     write(*,*) " "
     write(*,*) "In main.90: !!!!! GET PRE-DEFINED A from Simple_mod.F90 !!!!!"
     a = SimpleF__getA(s)
     write(*,*) "In main.90: a = ", a
+    ! new_char = SimpleF__getFstr(s)
+    ! write(*,*) "In main.90: get char... ", new_char
 
     write(*,*) " "
     write(*,*) "In main.90: !!!!! DEFINE AND GET NEW VARIABLES !!!!!"
@@ -36,3 +43,9 @@ program main_program
     ! b2 = SimpleF__getB(s,2,b)
     write(*,*) "In main.90: Defined values of b are ", b2
 end program main_program
+
+subroutine F2Cstring(Fstr,Cstr)
+    CHARACTER(256) :: Fstr
+    CHARACTER(256) :: Cstr
+    Cstr = Trim(Fstr)//char(0)
+end subroutine F2Cstring
