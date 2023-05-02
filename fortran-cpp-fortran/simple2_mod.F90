de7561ebcbb4b5e3cf81dcfe47c2cfb7dda8597e
Felipe_SC <100419439+CarvFS@users.noreply.github.com>
2023-05-02 11:45:00 -0700

    Add character object

--- fortran-cpp-fortran/simple2_mod.F90
+++ fortran-cpp-fortran/simple2_mod.F90
@@ -3,6 +3,7 @@ module simple2_mod
     TYPE :: SIMPLEF
       INTEGER :: a
       REAL :: dk
+      character,  dimension(256) :: Fstr
       ! Note that given the problem description, the component B 
       ! appears to have value semantics.  If so, as of Fortran 2003 
       ! this should be an ALLOCATABLE component.  Because it is 
