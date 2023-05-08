78d93f96e97ce5e83edba59592b411cd3d1dfaee
Felipe_SC <100419439+CarvFS@users.noreply.github.com>
2023-05-08 16:13:56 -0700

    Update main.F90

--- fortran-cpp-fortran/main.F90
+++ fortran-cpp-fortran/main.F90
@@ -2,16 +2,22 @@ program main_program
     use iso_c_binding
     use SimpleF_mod
     implicit none
-    integer :: a
+    integer :: a,bt,i,j
     integer :: new_a
     CHARACTER(256) :: Fstr = "Test for passing string F-Cpp-F"
     CHARACTER(256) :: Cstr, new_char, new_char_f
-    integer :: b(2), b2(2)
+    type(C_ptr) :: p
+    integer, target :: b(2),b2(2),b3(2)
+    integer, target :: c(2,2),c2(2,2)
     type(simp) :: s
     b(1) = 123
     b(2) = 543
-    ! write(*,*) "b = ", b
     
+    c(1,1) = 1
+    c(2,1) = 2
+    c(1,2) = 3
+    c(2,2) = 4
+
     call F2Cstring(Fstr,Cstr)
 
     write(*,*) "In main.90: !!!!! CREATING OBJECT !!!!!"
@@ -36,15 +42,33 @@ program main_program
     write(*,*) "In main.90: defining values for b as ", b
     call SimpleF__setB(s,2,b)
 
+    write(*,*) "In main.90: defining values for C as ", c
+    call SimpleF__setC(s,2,c)
+
     write(*,*) " "
     write(*,*) "In main.90: get new value for a..."
     a = SimpleF__getA(s)
     write(*,*) "In main.90: new a is ", a
 
     write(*,*) "In main.90: get values for b..."
-    call SimpleF__getB(s, 2, b2)
+    call SimpleF__getB(s, 2, b3)
     ! b2 = SimpleF__getB(s,2,b)
-    write(*,*) "In main.90: Defined values of b are ", b2
+    write(*,*) "In main.90: Defined values of b are ", b3
+
+    write(*,*) " "
+    write(*,*) "In main.F90: getting b values with function/subroutine steps: ", SimpleF__funsubgetB(s,2,b2)
+
+    write(*,*) " "
+    c2 = SimpleF__funsubgetC(s,2,c2)
+    write   (*,*) "In main.F90: getting C values with function/subroutine steps: ", c2
+
+    do i=1,2
+        do j=1,2
+            write(*,*) "index i,j = ", i,j, " => ", c2(i,j)
+        end do
+    end do
+
+
 end program main_program
 
 subroutine F2Cstring(Fstr,Cstr)
