de1a76054f40525b46b4e17f1d23648cb2ef2d89
Felipe_SC <100419439+CarvFS@users.noreply.github.com>
2023-05-12 10:12:46 -0700

    Update main.F90: receive arrays and pointers from c++

--- fortran-cpp-fortran/main.F90
+++ fortran-cpp-fortran/main.F90
@@ -6,24 +6,35 @@ program main_program
     integer :: new_a
     CHARACTER(256) :: Fstr = "Test for passing string F-Cpp-F"
     CHARACTER(256) :: Cstr, new_char, new_char_f
-    type(C_ptr) :: p
+    type(C_ptr) :: cptr
+    integer, pointer :: fptr(:),fptr2(:,:),M(:,:)
     integer, target :: b(2),b2(2),b3(2)
     integer, target :: c(2,2),c2(2,2)
+    integer :: N
+    logical :: logic
+    integer :: arr(100000)
+    integer, target :: matrix(100000,100000)
     type(simp) :: s
+
+    !> Defining values for b vector 
     b(1) = 123
     b(2) = 543
     
+    !> Defining values for c matrix
     c(1,1) = 1
     c(2,1) = 2
     c(1,2) = 3
     c(2,2) = 4
 
+    !> Add a null character at the end of the string, so c++ can have a stop point
     call F2Cstring(Fstr,Cstr)
 
+    !> Create object passing value for 'a' variable and 'Fstr' string
     write(*,*) "In main.90: !!!!! CREATING OBJECT !!!!!"
     write(*,*) "In main.90: defining Fstr as: ", Trim(Fstr), "..."
     call SimpleF__new(s,7654,Cstr)
 
+    !> Getting values asigned during the object creation
     write(*,*) " "
     write(*,*) "In main.90: !!!!! GET PRE-DEFINED A from Simple_mod.F90 !!!!!"
     a = SimpleF__getA(s)
@@ -31,9 +42,10 @@ program main_program
     new_char = "Test"
     call F2Cstring(new_char,new_char)
     call SimpleF__getFstr(s,new_char)
-    ! new_char = SimpleF__getFstr(s)
     write(*,*) "In main.90: get string = ", Trim(new_char), "..."
 
+    !> Changing value of 'a' variable and asigning values for 'b' vector and 'c' matrix
+    ! Defining values
     write(*,*) " "
     write(*,*) "In main.90: !!!!! DEFINE AND GET NEW VARIABLES !!!!!"
     new_a = 4321 
@@ -41,10 +53,10 @@ program main_program
     call SimpleF__setA(s, new_a)
     write(*,*) "In main.90: defining values for b as ", b
     call SimpleF__setB(s,2,b)
-
     write(*,*) "In main.90: defining values for C as ", c
     call SimpleF__setC(s,2,c)
 
+    ! getting values
     write(*,*) " "
     write(*,*) "In main.90: get new value for a..."
     a = SimpleF__getA(s)
@@ -52,7 +64,6 @@ program main_program
 
     write(*,*) "In main.90: get values for b..."
     call SimpleF__getB(s, 2, b3)
-    ! b2 = SimpleF__getB(s,2,b)
     write(*,*) "In main.90: Defined values of b are ", b3
 
     write(*,*) " "
@@ -67,6 +78,49 @@ program main_program
             write(*,*) "index i,j = ", i,j, " => ", c2(i,j)
         end do
     end do
+
+    !> FROM HERE ALL FUNTIONS/SUBROUTINES ARE COMMUNICATING IN A F90 -> C++ PROCESS, 
+    ! NOT IN F90 -> C++ -> F90 AS PREVIOUS CASES
+    ! I WILL BE WORKING ON THIS IN FUTURE EXAMPLES
+
+    !> Receiving arrays from c++
+    N=4 ! size for either vector or matrix dimensions
+    ! Receiving N*N array
+    call getArray(N, arr)
+    write(*,*) "In main.F90: Receiving array:"
+    do i=1,N*N
+        write(*,*) "i = ",i,"arr = ",arr(i)
+    end do
+    ! Turning it into a NxN matrix
+    write(*,*) "In main.F90: Turning it into a NxN matrix:"
+    do i = 1,N
+        do j = 1,N
+            matrix(i,j) = arr(N*(i-1) + j)
+            write(*,*) "(i j) = ",i,j,"  mat(i,j) = ",matrix(i,j)
+        end do
+    end do
+
+    !> Receiving pointers from c++
+    !! 1D case
+    call getPtr(N,cptr)
+    call C_F_POINTER(cptr,fptr,[N])
+    
+    write(*,*) "In main.F90: the vector received through a pointer is: ", fptr
+
+    !! 2D case
+    call get2DPtr(N,cptr)
+    call C_F_POINTER(cptr,fptr2,[N,N])
+
+    write(*,*) "In main.F90: the matrix received through a pointer is:"
+    do i=1,N
+        do j=1,N
+            write(*,*) "M[",i,",",j,"] = ", fptr2(i,j)
+        end do
+    end do
+
+    !> Receiving logical from c++
+    logic = getLogic(1)
+    write(*,*) "In main.F90: received boolean from c++ is ", logic
 
 
 end program main_program
