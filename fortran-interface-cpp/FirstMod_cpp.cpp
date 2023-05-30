bed716ba4c8f98bbde26301fc84e43723fb28e17
Felipe_SC <100419439+CarvFS@users.noreply.github.com>
2023-05-30 13:44:53 -0700

    Update FirstMod_cpp.cpp: Add string to 1st case and 1D array to 2nd

--- fortran-interface-cpp/FirstMod_cpp.cpp
+++ fortran-interface-cpp/FirstMod_cpp.cpp
@@ -5,26 +5,46 @@ using namespace std;
 extern "C" struct  firstex {
     int* a; 
     double* dk;
+    char* Fstr;
 };
 
 extern "C"{
-    void Initialize_firstex_v1(firstex* p, int a, double dk){
+    void Initialize_firstex_v1(firstex* p, int a, double dk, char* Fstr){
         cout << "In FirstMod_cpp.cpp's Initialize_firstex_v1: Data on C++ side!" << endl;
         cout << "In FirstMod_cpp.cpp's Initialize_firstex_v1: Receiving values to set..." << endl;
-        cout << "In FirstMod_cpp.cpp's Initialize_firstex_v1: a = " << a << ", dk = " << dk << endl;
+        cout << "In FirstMod_cpp.cpp's Initialize_firstex_v1: a = " << a << ", dk = " << dk << ", Fstr = " << Fstr << endl;
         
         cout << "In FirstMod_cpp.cpp's Initialize_firstex_v1: Printing pre-defined values:" << endl;
-        cout << "In FirstMod_cpp.cpp's Initialize_firstex_v1: a = " << *p->a << ", dk = " << *p->dk << endl;
-        
+        cout << "In FirstMod_cpp.cpp's Initialize_firstex_v1: a = " << *p->a << ", dk = " << *p->dk << endl; 
+        cout << "In FirstMod_cpp.cpp's Initialize_firstex_v1: Fstr is: ";
+        int i = 0;
+        while(p->Fstr[i] != '\0'){
+            cout << p->Fstr[i];
+            i++;
+        }
+        cout << endl;
         *p->a = a;
         *p->dk = dk;
 
-        // **p_Fstr = *Fstr;
+        i = 0;
+        while(Fstr[i] != '\0'){
+            p->Fstr[i] = Fstr[i];
+            i++;
+        }
+        p->Fstr[i] = '\0';
+
         cout << "In FirstMod_cpp.cpp's Initialize_firstex_v1: Printing new values:" << endl;
         cout << "In FirstMod_cpp.cpp's Initialize_firstex_v1: a = " << *p->a << ", dk = " << *p->dk << endl;
+        cout << "In FirstMod_cpp.cpp's Initialize_firstex_v1: Fstr = ";
+        i=0;
+        while(p->Fstr[i] != '\0'){
+            cout << p->Fstr[i];
+            i++;
+        }
+        cout << endl;
     }
 
-    void Initialize_firstex_v2(int** p_a, double** p_dk, char** p_Fstr, int a, double dk, char* Fstr){
+    void Initialize_firstex_v2(int** p_a, double** p_dk, char** p_Fstr, int** p_1dptr, int N, int a, double dk, char* Fstr){
         cout << "In FirstMod_cpp.cpp's Initialize_firstex_v2: Data on C++ side!" << endl;
         cout << "In FirstMod_cpp.cpp's Initialize_firstex_v2: Receiving values to set..." << endl;
         cout << "FirstMod_cpp.cpp's Initialize_firstex_v2: a = " << a << ", dk = " << dk << ", Fstr = " << Fstr << "..." << endl;
@@ -37,14 +57,20 @@ extern "C"{
 
         int i = 0;
         while(Fstr[i] != '\0'){
-            // cout << Fstr[i] << endl;
             p_Fstr[0][i] = Fstr[i];
             i++;
         }
         p_Fstr[0][i] = '\0';
 
-        // **p_Fstr = *Fstr;
         cout << "In FirstMod_cpp.cpp's Initialize_firstex_v2: Printing new values:" << endl;
         cout << "In FirstMod_cpp.cpp's Initialize_firstex_v2: a = " << **p_a << ", dk = " << **p_dk << ", Fstr = " << *p_Fstr << "..." << endl;
+
+        cout << "In FirstMod_cpp.cpp's Initialize_firstex_v2: receiving N = " << N << endl;
+
+        for(int i = 0; i < N; i++){
+            p_1dptr[0][i] = i+123;
+        }
+
+        cout << "In FirstMod_cpp.cpp's Initialize_firstex_v2: B = " << p_1dptr[0][0] << ", " << p_1dptr[0][1] << ", " << p_1dptr[0][2] << endl;
     }
 }
